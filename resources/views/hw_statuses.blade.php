bfaf57e0e0c7043e481dd711579c876594091ab9
fdmshd <kirh09764@gmail.com>
2022-11-26 13:42:31 +0700

    Initial commit

---
+++ resources/views/hw_statuses.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Homeworks</title>
+</head>
+
+<body>
+    <div class="card table-card">
+        <table>
+            <thead>
+                <th>Дз</th>
+                <th>Статус</th>
+                <th>Оценка</th>
+                <th>Deadline</th>
+            </thead>
+            <tbody>
+                @foreach ($homeworks as $homework)
+                <tr>
+                    <td>{{$homework->title}}</td>
+                    <td>{{$homework->status}}</td>
+                    <td>{{$homework->score}}</td>
+                    <td>{{$homework->dealine}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
